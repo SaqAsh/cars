643d43c11d51689bbe08c1e404762934805fdb1c
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2025-01-02 22:46:11 -0500

    format document

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,37 @@
 import express from "express";
-import http from 'http';
-import path from 'path';
+import http from "http";
+import path from "path";
 import { Server, Socket } from "socket.io";
-import { PlayerManager } from './player/PlayerManager';
+import { PlayerManager } from "./player/PlayerManager";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 const playerManager = new PlayerManager();
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+app.use(express.static(path.join(__dirname, "../public")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 function ManageConnections(): void {
-    io.on('connection', (socket: Socket) => {
+    io.on("connection", (socket: Socket) => {
         playerManager.HandleMovement(socket);
 
-        playerManager.users.push(socket); 
-        console.log(`User connected: ${socket.id}. Total: ${playerManager.users.length}`);
+        playerManager.users.push(socket);
+        console.log(
+            `User connected: ${socket.id}. Total: ${playerManager.users.length}`
+        );
 
         playerManager.HandleConnections();
-        socket.on('disconnect', () => {
+        socket.on("disconnect", () => {
             playerManager.users.splice(
-                playerManager.users.findIndex((user) => user.id === socket.id),1
+                playerManager.users.findIndex((user) => user.id === socket.id),
+                1
             );
-            console.log(`User disconnected: ${socket.id}. Total: ${playerManager.users.length}`);
+            console.log(
+                `User disconnected: ${socket.id}. Total: ${playerManager.users.length}`
+            );
             playerManager.HandleConnections();
         });
     });
@@ -34,6 +39,5 @@ function ManageConnections(): void {
 
 ManageConnections();
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+    console.log("listening on *:3000");
 });
-
--- src/player/PlayerManager.ts
+++ src/player/PlayerManager.ts
@@ -3,7 +3,6 @@
 import { Socket } from "socket.io";
 
 export class PlayerManager {
-
     public users: Socket[];
 
     constructor() {
@@ -12,41 +11,46 @@ export class PlayerManager {
 
     public HandleConnections(): void {
         if (this.users.length === 2) {
-            for ( let i = 0; i < 2; i++){
-                if(this.users[i].rooms.has('waiting')) this.users[i].rooms.delete('waiting'); // in the case that the user was in the waiting room before they joined we are gonna remove the waiting stage
+            for (let i = 0; i < 2; i++) {
+                if (this.users[i].rooms.has("waiting"))
+                    this.users[i].rooms.delete("waiting"); // in the case that the user was in the waiting room before they joined we are gonna remove the waiting stage
                 this.users[i].join("game");
-                this.users[i].emit('waitingLobby', { message: 'Welcome to the game!' });
+                this.users[i].emit("waitingLobby", {
+                    message: "Welcome to the game!",
+                });
                 console.log(this.users[i].rooms);
-            }          
+            }
         } else if (this.users.length > 2) {
             for (let i = 2; i < this.users.length; i++) {
                 this.users[i].join("waiting");
-                this.users[i].emit('waitingLobby', { message: 'You are in the waiting lobby. Please wait for a player to disconnect.' });
+                this.users[i].emit("waitingLobby", {
+                    message:
+                        "You are in the waiting lobby. Please wait for a player to disconnect.",
+                });
                 console.log(this.users[i].rooms);
             }
         }
     }
 
-    public HandleMovement(socket: Socket): void{
-        socket.on("keypress", (arg : any)=>{
-            if(this.users.length < 2){
+    public HandleMovement(socket: Socket): void {
+        socket.on("keypress", (arg: any) => {
+            if (this.users.length < 2) {
                 return;
             }
-            if(Object.keys(arg)[0] === this.users[0].id){
+            if (Object.keys(arg)[0] === this.users[0].id) {
                 socket.emit("car1", 20);
             }
-            if(Object.keys(arg)[0] === this.users[1].id){
+            if (Object.keys(arg)[0] === this.users[1].id) {
                 socket.emit("car2", 20);
             }
         });
 
-        socket.on("car1Position", (arg:any) =>{
+        socket.on("car1Position", (arg: any) => {
             this.users[1].emit("car1Position", arg);
-        })
+        });
 
-        socket.on("car2Position", (arg:any) =>{
+        socket.on("car2Position", (arg: any) => {
             this.users[0].emit("car2Position", arg);
-        })
+        });
     }
-
 }
